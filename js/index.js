255412ffff534b6f4d6dbcd5e9a98cb4ccf149fa
flare-s <webflarez@gmail.com>
2022-04-19 17:33:36 +0100

    Add tilt effect to the projects elements

--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ const header = document.querySelector(".header");
 let lastScroll;
 
 
+
+// Hiding the navbar when scrolling down and showing it when scrolling up
 window.addEventListener("scroll", () => {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if (scrollTop > lastScroll) {
@@ -14,7 +16,6 @@ window.addEventListener("scroll", () => {
     } else {
         header.style.top = "0";
     }
-    console.log(scrollTop ,projects.offsetTop);
     lastScroll = scrollTop;
 
 })
@@ -22,18 +23,19 @@ window.addEventListener("scroll", () => {
 
 
 
-
+// custom mouse 
 window.addEventListener("mousemove", (e) => {
     let newTrail = document.createElement("div");
-    // newTrail.setAttribute("aria-hidden", "true");
+    newTrail.setAttribute("aria-hidden", "true");
     newTrail.classList.add("trail");
     newTrail.style.left = `${e.clientX}px;`;
     newTrail.style.top = `${e.clientY}px;`;
     newTrail.setAttribute("style",`top: ${e.clientY - 15}px; left: ${e.clientX - 15}px;`);
+    //deleting the created element when the animation ends
     newTrail.onanimationend = () => newTrail.remove();
     document.body.prepend(newTrail);
 
-})
+});
 
 // adding Intersection Observer polyfill if it's not supported by the browser
 if (!'IntersectionObserver' in window ||
@@ -107,7 +109,36 @@ let sectionObserver = new IntersectionObserver(entries => {
     }
 }, null);
 
-project.forEach(el => sectionObserver.observe(el))
+
+// tilt effict on elements when the mouse is moved on them
+const tiltEl = (e, el) => {
+        const target = el;
+        const targetRect = target.getBoundingClientRect();
+        const targetWidth = targetRect.width;
+        const targetHeight = targetRect.height;
+        // getting the position of the center of the element horizontally and vertically
+        let targetXCenter = targetRect.left + targetWidth/2;
+        let targetYCenter = targetRect.top + targetHeight/2;
+        // getting the position of the mouse relative to the center of the element horizontally and vertically
+        let mouseX = e.clientX - targetXCenter;
+        let mouseY = e.clientY - targetYCenter;
+        // setting the limit of the tilt effect when the mouse move on the element
+        let rotateX = (15 * mouseY / (targetHeight/2));
+        let rotateY = (-15 * mouseX / (targetWidth/2));
+
+        target.style.transform = `scale(1) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+
+
+    
+}
+
+project.forEach(el => { 
+    sectionObserver.observe(el);
+    el.addEventListener("mousemove", (e) => tiltEl(e, el));
+    el.addEventListener("mouseleave", () => el.style.transform = `scale(1) rotateX(0deg) rotateY(0deg)`);
+
+})
 
 
 
